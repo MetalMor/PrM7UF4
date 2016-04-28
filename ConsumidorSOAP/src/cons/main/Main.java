f72d7cb134d38d61c24970d6b1a5c931d9f972ea
mor <srgark@gmail.com>
2016-04-28 13:27:56 +0200

    init

---
+++ ConsumidorSOAP/src/cons/main/Main.java
@@ -0,0 +1,19 @@
+package cons.main;
+
+/**
+ * Clase principal del consumidor, para ejecutarlo.
+ *
+ * @author mor
+ * @version 280416
+ */
+public class Main {
+
+    /**
+     * Método principal del consumidor
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO consumidor
+    }
+    
+}
